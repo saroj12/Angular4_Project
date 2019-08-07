95addc3dc9e60ca7d4286a5272a96d23b0a6cf40
Saroj Sharma <sarojkanta.sharma@gmail.com>
2019-08-07 21:34:53 +0530

    updated the new files

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',<courses></courses>
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = ' angular app works!';
+}
